0812a4c3f9f1287fb50c17c13ea63b16055d6df8
Nevermind23 <tyeshela7@gmail.com>
2022-12-12 11:42:41 +0400

    Minor bugfix

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', response()->json(["Covid-stats API"]));
+Route::get('/', function () {
+    return response()->json(["Covid-stats API"]);
+});
